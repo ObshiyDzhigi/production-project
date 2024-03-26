88c14d80b7dec7f4e2d1921247b8957938cd9df7
zhigazh2017@outlook.com <zhigazh2017@outlook.com>
2024-03-26 17:06:16 +0500

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildPlugins} from "./buildPlugins";
+import {buildLoaders} from "./buildLoaders";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions):webpack.Configuration{
+    const {paths,mode,isDev} = options
+    return {
+        mode:mode,
+        entry: paths.entry,// СТАРТОВАЯ ТОЧКА НАШЕГО ПРИЛОЖЕНИЯ
+        //НАСТРОЙКА ТОГО КУДА И КАК МЫ БУДЕМ ДЕЛАТЬ СБОРКУ НАШЕГО ПРИЛОЖЕНИЯ
+        output:{
+            filename:"[name].[contenthash].js", //ГЛАВНЫЙ ФАЙЛ В СБОРКЕ НАШЕГО ПРИЛОЖЕНИЯ
+            path:paths.build,
+            clean: true
+        },
+        plugins:buildPlugins(options),
+        module: {
+            rules: buildLoaders(options)
+        },
+        resolve: buildResolvers(),
+        devtool:isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
+}
